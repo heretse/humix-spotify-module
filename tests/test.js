2d0b6b5d63431ad4b50590d0ffd98edafe8f6d5c
winston_hsieh <winston_hsieh@gemteks.com>
2017-03-03 17:25:17 +0800

    Add event on playing, playend and error.

--- tests/test.js
+++ tests/test.js
@@ -4,4 +4,5 @@ var spotify = new SpotifyPlayer({clientId: 'b6ca0e1423e74fae862f59b320e646f2',
     clientSecret: 'a5b0c12d8ea34bedb63d3bde12cb51c9'});
 spotify.init();
 
+spotify.playSong("seasons in the sun");
 spotify.playSong("Shape of You", "Ed Sheeran");
