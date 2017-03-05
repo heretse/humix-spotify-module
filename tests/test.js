0a54beed571ffb675ef63c0149011dde3ffde3cd
winston_hsieh <winston_hsieh@gemteks.com>
2017-03-05 11:08:35 +0800

    Redefine playing sequence for the Spotify player.

--- tests/test.js
+++ tests/test.js
@@ -1,8 +1,22 @@
+
 var SpotifyPlayer = require('../index');
 
 var spotify = new SpotifyPlayer({spotify:{clientId: 'b6ca0e1423e74fae862f59b320e646f2',
     clientSecret: 'a5b0c12d8ea34bedb63d3bde12cb51c9'}});
 spotify.init();
 
-spotify.playSong("seasons in the sun");
-spotify.playSong("Shape of You", "Ed Sheeran");
+// wait for setting access token
+var myVar = setInterval(() => {
+    
+    spotify.playSong("seasons in the sun");
+    clearInterval(myVar);
+
+}, 3000);
+
+// Add another song after 3 seconds
+var myVar2 = setInterval(() => {
+
+    spotify.playSong("Shape of You", "Ed Sheeran");
+    clearInterval(myVar2);
+
+}, 10000);
